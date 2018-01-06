4151f1fc3520f0829009c6a56a8fd4eadc98957a
Andrew <33483980+abell95@users.noreply.github.com>
2018-01-05 17:17:05 -0800

    Create random_email.go

---
+++ random_email.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+//only creates all-caps emails- variable lettering tbd
+func randomEmail(nLength int, host, suffix string) string {
+	rand.Seed(time.Now().UTC().UnixNano())
+	
+	bytes := []byte{}
+	randEmail := ""
+	
+	for i := 0; i < nLength; i++ {
+		var byteAppended uint8 = 0
+		rands := func() uint8 {
+			min, max := 65, 90
+			val := min + rand.Intn(max - min)
+			return uint8(val)
+		}
+		byteAppended = rands()
+		bytes = append(bytes, byteAppended)
+	}
+	
+	randEmail = string(bytes[:]) + "@" + host + "." + suffix
+	
+	return randEmail
+}
+
+func main() {
+	fmt.Println(randomEmail(12, "yahoo", "com"))
+	fmt.Println(randomEmail(8, "bigbusiness", "net"))
+    fmt.Println(randomEmail(22, "contractor", "gov"))
+}
